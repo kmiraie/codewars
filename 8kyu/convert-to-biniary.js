11e6475957efb82400760d2c8e405b961590721a
Kavon Miraie <kavonmiraie@yahoo.com>
2024-04-05 12:00:00 +0900

    added a new codewars solution

---
+++ 8kyu/convert-to-biniary.js
@@ -0,0 +1,7 @@
+// Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+//My Solution
+
+function toBinary(n){
+    return +n.toString(2);
+  }
