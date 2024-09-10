0cbe63bfec984f7367bdff16e5b4355e48f0b4f5
Kavon Miraie <kavonmiraie@yahoo.com>
2024-09-10 11:58:42 +0900

    added a new codewars solution

---
+++ 7kyu/factorial-two.js
@@ -0,0 +1,11 @@
+// Your task is to write function factorial.
+
+//My Solution
+
+function factorial(n){
+    let num = 1
+    for(let i = 1; i<=n; i++) {
+      num *= i
+    }
+    return num
+  }
